477db81ed9f32a06a6328266bc7e39e9cf992810
Sahar654321 <sahar.almohamady@gmail.com>
2023-09-03 02:51:57 -0700

    add tests for pager

---
+++ app/src/test/java/com/sahar/aflamy/data/repository/remote/MoviesPagingTest.kt
@@ -0,0 +1,26 @@
+package com.sahar.aflamy.data.repository.remote
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class MoviesPagingTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun getRefreshKey() {
+    }
+
+    @Test
+    fun load() {
+    }
+}
